a3eea5181817a5daba60266f7eb8b9f04960eb4f
sincherer <ersincher@gmail.com>
2025-03-09 15:40:49 +0800

    Update main.tsx and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/hippo/',
-
-  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
