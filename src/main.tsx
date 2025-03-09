ca7f9614cf726a38f9c41eeb316e66baaac4141a
sincherer <ersincher@gmail.com>
2025-03-09 14:07:04 +0800

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/hippo">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
