b36796628f0e1717d792cc1df44357cf752588c8
sincherer <ersincher@gmail.com>
2025-03-10 18:07:05 +0800

    Updates

--- src/App.module.css.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly [key: string]: string;
-};
-
-export default styles;
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/hippo/',  // This should match your GitHub repo name
-})
